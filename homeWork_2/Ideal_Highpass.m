b3ded2564a86ef72e3dc3df5f465575a61105266
haohao <1062131664@qq.com>
2017-11-16 09:17:44 +0800

    hand in homework
    homeWork2、homeWork3

---
+++ homeWork_2/Ideal_Highpass.m
@@ -0,0 +1,72 @@
+close all;
+clear all;
+f = imread('cameraman.tif');
+f = mat2gray(f,[0 255]);
+
+[M,N] = size(f);
+P = 2*M;
+Q = 2*N;
+fc = zeros(M,N);
+
+for x = 1:1:M
+    for y = 1:1:N
+        fc(x,y) = f(x,y) * (-1)^(x+y);
+    end
+end
+
+F = fft2(fc,P,Q);
+
+H_1 = zeros(P,Q);
+H_2 = zeros(P,Q);
+H_3 = zeros(P,Q);
+
+for x = (-P/2):1:(P/2)-1
+     for y = (-Q/2):1:(Q/2)-1
+        D = (x^2 + y^2)^(0.5);
+        if(D >= 10)  H_1(x+(P/2)+1,y+(Q/2)+1) = 1; end    
+        if(D >= 50)  H_2(x+(P/2)+1,y+(Q/2)+1) = 1; end
+        if(D >= 150)  H_3(x+(P/2)+1,y+(Q/2)+1) = 1; end
+     end
+end
+
+G_1 = H_1 .* F;
+G_2 = H_2 .* F;
+G_3 = H_3 .* F;
+
+g_1 = real(ifft2(G_1));
+g_1 = g_1(1:1:M,1:1:N);
+
+g_2 = real(ifft2(G_2));
+g_2 = g_2(1:1:M,1:1:N);         
+
+g_3 = real(ifft2(G_3));
+g_3 = g_3(1:1:M,1:1:N);  
+
+for x = 1:1:M
+    for y = 1:1:N
+        g_1(x,y) = g_1(x,y) * (-1)^(x+y);
+        g_2(x,y) = g_2(x,y) * (-1)^(x+y);
+        g_3(x,y) = g_3(x,y) * (-1)^(x+y);
+    end
+end
+
+%% -----show-------
+figure();
+subplot(2,2,1);
+imshow(f,[0 1]);
+xlabel('a).Original Image');
+
+subplot(2,2,2);
+imshow(g_1,[0 1]);
+xlabel('b).Ideal Highpass/D0=10');
+
+subplot(2,2,3);
+imshow(g_2,[0 1]);
+xlabel('c).Ideal Highpass/D0=50');
+
+subplot(2,2,4);
+imshow(g_3,[0 1]);
+xlabel('d).Ideal Highpass/D0=150');
+
+
+
---
+++ homwWork_3/Ari_MeanFilter.m
@@ -0,0 +1,50 @@
+close all 
+clear all
+image=imread('filtering.tif');
+
+figure();
+subplot(2,2,1);
+imshow(image);
+xlabel('Raw image');
+
+image_mean_3 = arithmetic_mean_filter(image,3);
+subplot(2,2,2);
+imshow(image_mean_3)
+xlabel('arithmetic mean filter/3');
+
+image_mean_5 = arithmetic_mean_filter(image,5);
+subplot(2,2,3);
+imshow(image_mean_5)
+xlabel('arithmetic mean filter/5');
+
+image_mean_9 = arithmetic_mean_filter(image,9);
+subplot(2,2,4);
+imshow(image_mean_9)
+xlabel('arithmetic mean/9');
+
+%%
+function [result_imag]= arithmetic_mean_filter(image,filter_size)
+    Ex_image = extend_imag(image);
+    result_imag = image;
+    [M,N]=size(Ex_image);
+    helf_size = (filter_size-1)/2;
+    
+    for x=1+helf_size:1:M-helf_size
+        for y=1+helf_size:1:N-helf_size
+            slid=Ex_image(x-helf_size:1:x+helf_size,y-helf_size:1:y+helf_size);     
+            result_imag(x,y)=sum(slid(:))/numel(slid); 
+        end
+    end
+end
+    
+%%
+function [result_imag] = extend_imag(image)
+    [M,N] = size(image);
+    temp = zeros(M+2,N+2);
+    for x = 1:M
+        temp(x+1,:) = [image(x,1) image(x,:) image(x,N)];
+    end
+    temp(1,:) = temp(2,:);
+    temp(M+2,:) = temp(M+1,:);
+    result_imag = temp;
+end
